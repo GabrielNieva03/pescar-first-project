5bf27047ed27a39d24823b2885e17268a8812622
Gabriel Nieva <gabynieva33@gmail.com>
2021-10-18 18:11:11 -0300

    obtener novedades de mock

--- news/js/news.js
+++ news/js/news.js
@@ -8,15 +8,15 @@ class Novedad {
   }
 
   var novedades = [];
-  novedades.push(new Novedad(1, "Se casó la hija de tinelli", "9/9 10 HS", "Espectaculo"));
-  novedades.push(new Novedad(2, "Susana jimenes arranca su programa en 2 dias", "9/9 10 HS", "Espectaculo"));
-  novedades.push(new Novedad(3, "Lo que sea ejemplo de novedad", "9/9 10 HS", "Noticias"));
-  novedades.push(new Novedad(4, "Tinelli se compró un auto nuevo", "9/9 10 HS", "Espectaculo"));
+  //novedades.push(new Novedad(1, "Se casó la hija de tinelli", "9/9 10 HS", "Espectaculo"));
+  //novedades.push(new Novedad(2, "Susana jimenes arranca su programa en 2 dias", "9/9 10 HS", "Espectaculo"));
+  //novedades.push(new Novedad(3, "Lo que sea ejemplo de novedad", "9/9 10 HS", "Noticias"));
+  //novedades.push(new Novedad(4, "Tinelli se compró un auto nuevo", "9/9 10 HS", "Espectaculo"));
 
   function enviandoFormulario() {
     document.getElementById('result').innerHTML = '';
     var value = document.getElementById('input-search').value.toLocaleUpperCase();
-    
+   
     var result = "<ul>";
     
     novedades.forEach(novedad => {
@@ -29,4 +29,21 @@ class Novedad {
     result += "</ul>";
     
     document.getElementById('result').insertAdjacentHTML("beforeend", result);
+
   }
+   
+  function obtenerJsonApi() {
+    fetch('https://mocki.io/v1/43f89ab7-76ec-4ae1-b3ee-539a130b459e')
+    .then(response => {
+        console.log("Gabi RESPONSE");
+        console.log(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log("Gaby JSON");
+        console.log(data);
+        novedades = data;
+    });
+  }
+
+ obtenerJsonApi();
